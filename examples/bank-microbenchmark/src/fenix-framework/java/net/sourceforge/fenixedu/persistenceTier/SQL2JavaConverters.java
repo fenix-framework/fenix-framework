540d53486d161d60b7379c65d91c90b3c937f446
João Cachopo <joao.cachopo@ist.utl.pt>
2009-05-04 16:11:53 +0000

    Remove classes from the bank-microbenchmark example that are no longer needed.

--- examples/bank-microbenchmark/src/fenix-framework/java/net/sourceforge/fenixedu/persistenceTier/SQL2JavaConverters.java
+++
@@ -1,7 +0,0 @@
-package net.sourceforge.fenixedu.persistenceTier;
-
-import org.apache.ojb.broker.accesslayer.conversions.FieldConversion;
-
-public class SQL2JavaConverters {
-    public static FieldConversion pt$ist$fenixframework$example$bankbench$ff$MoneyConverter = new pt.ist.fenixframework.example.bankbench.ff.MoneyConverter();
-}
