5e1b828dfa8e984ee25da57b659bc45964316299
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-07-31 12:06:05 +0000

    Method isWriteTransaction() became public instead of protected.

--- src/java/pt/ist/fenixframework/pstm/ReadOnlyTopLevelTransaction.java
+++ src/java/pt/ist/fenixframework/pstm/ReadOnlyTopLevelTransaction.java
@@ -3,50 +3,49 @@ package pt.ist.fenixframework.pstm;
 import jvstm.VBoxBody;
 import jvstm.ResumeException;
 
-
 class ReadOnlyTopLevelTransaction extends TopLevelTransaction {
 
     ReadOnlyTopLevelTransaction(jvstm.ActiveTransactionsRecord record) {
-        super(record);
+	super(record);
     }
 
     @Override
     protected void checkValidity(jvstm.ActiveTransactionsRecord record) {
-        // for read-only transactions, for which we do not store the
-        // read-set, it is not possible to know that we will see a
-        // consistent read after resuming, unless the new record is
-        // exactly the same that we have
+	// for read-only transactions, for which we do not store the
+	// read-set, it is not possible to know that we will see a
+	// consistent read after resuming, unless the new record is
+	// exactly the same that we have
 
-        if (record != this.activeTxRecord) {
-            throw new ResumeException("Transaction may be no longer valid for resuming");
-        }
+	if (record != this.activeTxRecord) {
+	    throw new ResumeException("Transaction may be no longer valid for resuming");
+	}
     }
 
     protected void initDbChanges() {
-        // do nothing
+	// do nothing
     }
 
     public ReadSet getReadSet() {
-        throw new Error("ReadOnly txs don't record their read sets...");
+	throw new Error("ReadOnly txs don't record their read sets...");
     }
 
     public <T> T getBoxValue(VBox<T> vbox, Object obj, String attr) {
-        numBoxReads++;
-        VBoxBody<T> body = vbox.body.getBody(number);
-        if (body.value == VBox.NOT_LOADED_VALUE) {
-            vbox.reload(obj, attr);
-            // after the reload, the same body should have a new value
-            // if not, then something gone wrong and its better to abort
-            if (body.value == VBox.NOT_LOADED_VALUE) {
-                System.out.println("Couldn't load the attribute " + attr + " for class " + obj.getClass());
-                throw new VersionNotAvailableException();
-            }
-        }
+	numBoxReads++;
+	VBoxBody<T> body = vbox.body.getBody(number);
+	if (body.value == VBox.NOT_LOADED_VALUE) {
+	    vbox.reload(obj, attr);
+	    // after the reload, the same body should have a new value
+	    // if not, then something gone wrong and its better to abort
+	    if (body.value == VBox.NOT_LOADED_VALUE) {
+		System.out.println("Couldn't load the attribute " + attr + " for class " + obj.getClass());
+		throw new VersionNotAvailableException();
+	    }
+	}
 
-        return body.value;
+	return body.value;
     }
-    
-    protected boolean isWriteTransaction() {
-        return false;
+
+    public boolean isWriteTransaction() {
+	return false;
     }
 }
