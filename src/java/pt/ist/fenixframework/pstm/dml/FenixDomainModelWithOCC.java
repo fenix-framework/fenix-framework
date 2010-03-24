e3a2e252ccd4a4b56e6dbce7ae66db322ff67654
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2010-03-24 16:59:47 +0000

    Bug fix. This wasn't overriding the correct method since the external keyword refactorization.

--- src/java/pt/ist/fenixframework/pstm/dml/FenixDomainModelWithOCC.java
+++ src/java/pt/ist/fenixframework/pstm/dml/FenixDomainModelWithOCC.java
@@ -9,52 +9,52 @@ import dml.Slot;
 public class FenixDomainModelWithOCC extends FenixDomainModel {
 
     /*
-     * This must be reevaluated.  In Fenix there are several cases of
-     * relations that are differentiations of another relation.
+     * This must be reevaluated. In Fenix there are several cases of relations
+     * that are differentiations of another relation.
      * 
-     * For instance, there are many relations between the
-     * RootDomainObject and other classes, but there is only one
-     * keyRootDomainObject in the DomainObject class, rather than one
-     * for each subclass.
-     *
-     * I guess that the proper way to deal with this was to declare an
-     * abstract relation between RootDomainObject and DomainObject,
-     * and then add concrete relations between the different
-     * subclasses of DomainObject and the RootDomainObject.
-     *
-     * This, however, needs further brainstorming...
-     * So, I will leave this disabled for now.
+     * For instance, there are many relations between the RootDomainObject and
+     * other classes, but there is only one keyRootDomainObject in the
+     * DomainObject class, rather than one for each subclass.
+     * 
+     * I guess that the proper way to deal with this was to declare an abstract
+     * relation between RootDomainObject and DomainObject, and then add concrete
+     * relations between the different subclasses of DomainObject and the
+     * RootDomainObject.
+     * 
+     * This, however, needs further brainstorming... So, I will leave this
+     * disabled for now.
      */
-    
-    @Override
-    public void finalizeDomain() {
-        super.finalizeDomain();
-        
-        for (final DomainClass domainClass : classes.values()) {
-            final int domainClassHierarchyLevel = calculateHierarchyLevel(domainClass);
-            if (domainClassHierarchyLevel > 1) {
-                final DomainClass domainObjectDescendent = findDirectDomainObjectDecendent(domainClass);
-                final Slot ojbConcreteClassSlot = domainObjectDescendent.findSlot("ojbConcreteClass");
-                if (ojbConcreteClassSlot == null) {
-                    domainObjectDescendent.addSlot(new Slot("ojbConcreteClass", findValueType("String")));
-                }
-            }
-        }
 
-        checkForRepeatedSlots();
+    @Override
+    public void finalizeDomain(boolean checkForMissingExternals) {
+	super.finalizeDomain(checkForMissingExternals);
+
+	for (final DomainClass domainClass : classes.values()) {
+	    final int domainClassHierarchyLevel = calculateHierarchyLevel(domainClass);
+	    if (domainClassHierarchyLevel > 1) {
+		final DomainClass domainObjectDescendent = findDirectDomainObjectDecendent(domainClass);
+		final Slot ojbConcreteClassSlot = domainObjectDescendent.findSlot("ojbConcreteClass");
+		if (ojbConcreteClassSlot == null) {
+		    domainObjectDescendent.addSlot(new Slot("ojbConcreteClass", findValueType("String")));
+		}
+	    }
+	}
+
+	checkForRepeatedSlots();
     }
 
     private DomainClass findDirectDomainObjectDecendent(final DomainClass domainClass) {
-        final int domainClassHierarchyLevel = calculateHierarchyLevel(domainClass);
-        return domainClassHierarchyLevel == 1 ? domainClass : findDirectDomainObjectDecendent((DomainClass) domainClass.getSuperclass());
+	final int domainClassHierarchyLevel = calculateHierarchyLevel(domainClass);
+	return domainClassHierarchyLevel == 1 ? domainClass : findDirectDomainObjectDecendent((DomainClass) domainClass
+		.getSuperclass());
     }
 
     private int calculateHierarchyLevel(final DomainClass domainClass) {
-        final DomainEntity domainEntity = domainClass.getSuperclass();
-        return domainEntity == null || !isDomainClass(domainEntity) ? 0 : calculateHierarchyLevel((DomainClass) domainEntity) + 1;
+	final DomainEntity domainEntity = domainClass.getSuperclass();
+	return domainEntity == null || !isDomainClass(domainEntity) ? 0 : calculateHierarchyLevel((DomainClass) domainEntity) + 1;
     }
-    
+
     private boolean isDomainClass(final DomainEntity domainEntity) {
-        return domainEntity instanceof DomainClass;
+	return domainEntity instanceof DomainClass;
     }
 }
