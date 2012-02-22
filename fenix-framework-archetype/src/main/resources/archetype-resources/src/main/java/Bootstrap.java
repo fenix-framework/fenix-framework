2b5426e2cbd480e731c6c2f71c0f39db8bb2b2a7
David Martinho <davidmartinho@gmail.com>
2012-02-22 18:06:00 +0000

    FenixFramework Archetype is now generating FenixFramework basic applications that actually compile and work

---
+++ fenix-framework-archetype/src/main/resources/archetype-resources/src/main/java/Bootstrap.java
@@ -0,0 +1,22 @@
+package ${package};
+
+import pt.ist.fenixframework.Config;
+import pt.ist.fenixframework.FenixFramework;
+import ${package}.domain.${rootClassname};
+
+public class Bootstrap {
+
+  public static void init() {
+    try {
+      FenixFramework.initialize(new Config() {{
+        domainModelPath = "/${artifactId}.dml";
+        dbAlias = "//localhost:3306/${dbName}";
+        dbUsername = "${dbUsername}";
+        dbPassword = "";
+        rootClass = ${rootClassname}.class;
+      }});
+    } catch(Error e) {
+      
+    }
+  }
+}
