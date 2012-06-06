fbd224cb075009076930da7d5386d5b9ff591acb
David Martinho <davidmartinho@gmail.com>
2012-06-06 20:12:16 +0100

    Bug Fix: DmlMojo and PostCompileMojo now ignore projects that do not have a src/main/dml folder, while still considering external jar contained files

--- dml-maven-plugin/src/main/java/dml/maven/DmlMojo.java
+++ dml-maven-plugin/src/main/java/dml/maven/DmlMojo.java
@@ -17,137 +17,142 @@ import org.codehaus.plexus.util.DirectoryScanner;
  * Generate base classes from the DML files
  *
  * @goal generate-domain
- * 
+ *
  * @phase generate-sources
  *
  * @requiresDependencyResolution runtime
  */
 public class DmlMojo extends AbstractMojo {
 
-  /**
-   * Maven Project
-   * @parameter default-value="${project}"
-   */
-  private MavenProject mavenProject;
+    /**
+     * Maven Project
+     * @parameter default-value="${project}"
+     */
+    private MavenProject mavenProject;
+    /**
+     * File Source Directory
+     * @parameter expression="${generate-domain.src}" default-value="${basedir}/src/main/dml"
+     */
+    private File srcDirectoryFile;
+    /**
+     * File Destination Directory
+     * @parameter expression="${generate-domain.dest}" default-value="${basedir}/src/main/java"
+     */
+    private File destDirectoryFile;
+    /**
+     * Base File Destination Directory
+     * @parameter expression="${generate-domain.destBase}" default-value="${project.build.directory}/generated-sources/dml-maven-plugin"
+     */
+    private File destDirectoryBaseFile;
+    /**
+     * Domain Model Class Name
+     * @parameter expression="${generate-domain.domainModelClassName}" default-value="pt.ist.fenixframework.pstm.dml.FenixDomainModel"
+     */
+    private String domainModelClassName;
+    /**
+     * Code Generator Class Name
+     * @parameter expression="${generate-domain.codeGeneratorClassName}" default-value="pt.ist.fenixframework.pstm.dml.FenixCodeGeneratorOneBoxPerObject"
+     */
+    private String codeGeneratorClassName;
+    /**
+     * Package name
+     * @parameter expression="${generate-domain.packageName}" default-value=""
+     */
+    private String packageName;
+    /**
+     * Generate Finals Flag
+     * @parameter expression="${generate-domain.generateFinals}" default-value="false"
+     */
+    private boolean generateFinals;
+    /**
+     * Verbose Mode Flag
+     * @parameter expression="${generate-domain.verbose}" default-value="false"
+     */
+    private boolean verbose;
 
-  /**
-   * File Source Directory
-   * @parameter expression="${generate-domain.src}" default-value="${basedir}/src/main/dml"
-   */
-  private File srcDirectoryFile;
-	
-  /**
-   * File Destination Directory
-   * @parameter expression="${generate-domain.dest}" default-value="${basedir}/src/main/java"
-   */
-  private File destDirectoryFile;
+    public void execute() throws MojoExecutionException {
+        if (mavenProject.getArtifact().getType().equals("pom")) {
+            getLog().info("Cannot generate domain for pom projects");
+            return;
+        }
 
-  /**
-   * Base File Destination Directory
-   * @parameter expression="${generate-domain.destBase}" default-value="${project.build.directory}/generated-sources/dml-maven-plugin"
-   */
-  private File destDirectoryBaseFile;
+        CompilerArgs compArgs = null;
+        List<String> domainSpecFileNames = new ArrayList<String>();
+        long latestBuildTime = destDirectoryBaseFile.lastModified();
 
-  /**
-   * Domain Model Class Name
-   * @parameter expression="${generate-domain.domainModelClassName}" default-value="pt.ist.fenixframework.pstm.dml.FenixDomainModel"
-   */
-  private String domainModelClassName;
+        List<String> dmlFileList = DmlMojoUtils.readDmlFilePathsFromArtifact(getLog(), mavenProject.getArtifacts());
+        domainSpecFileNames.addAll(dmlFileList);
+        boolean shouldCompile = false;
 
-  /**
-   * Code Generator Class Name
-   * @parameter expression="${generate-domain.codeGeneratorClassName}" default-value="pt.ist.fenixframework.pstm.dml.FenixCodeGeneratorOneBoxPerObject"
-   */
-  private String codeGeneratorClassName;
+        if (srcDirectoryFile.exists()) {
 
-  /**
-   * Package name
-   * @parameter expression="${generate-domain.packageName}" default-value=""
-   */
-  private String packageName;
 
-  /**
-   * Generate Finals Flag
-   * @parameter expression="${generate-domain.generateFinals}" default-value="false"
-   */
-  private boolean generateFinals;
+            DirectoryScanner scanner = new DirectoryScanner();
+            scanner.setBasedir(this.srcDirectoryFile);
 
-  /**
-   * Verbose Mode Flag
-   * @parameter expression="${generate-domain.verbose}" default-value="false"
-   */
-  private boolean verbose;
+            String[] includes = {"**\\*.dml"};
+            scanner.setIncludes(includes);
+            scanner.scan();
 
-  public void execute() throws MojoExecutionException {
-		
-    CompilerArgs compArgs = null;
-    List<String> domainSpecFileNames = new ArrayList<String>();
-    long latestBuildTime = destDirectoryBaseFile.lastModified();
 
-    DirectoryScanner scanner = new DirectoryScanner();
-    scanner.setBasedir(this.srcDirectoryFile);
-    
-    String[] includes = {"**\\*.dml"};
-    scanner.setIncludes(includes);
-    scanner.scan();
-    
-    boolean shouldCompile = false;
+            Resource resource = new Resource();
+            resource.setDirectory(this.srcDirectoryFile.getAbsolutePath());
+            resource.addInclude("*.dml");
+            mavenProject.addResource(resource);
 
-    Resource resource = new Resource();
-    resource.setDirectory(this.srcDirectoryFile.getAbsolutePath());
-    resource.addInclude("*.dml");
-    mavenProject.addResource(resource);
-    
-    List<String> dmlFileList = DmlMojoUtils.readDmlFilePathsFromArtifact(getLog(), mavenProject.getArtifacts());
-    domainSpecFileNames.addAll(dmlFileList);
-    
-    String[] includedFiles = scanner.getIncludedFiles();
-    for (String includedFile : includedFiles) {
-      String filePath = this.srcDirectoryFile.getAbsolutePath() + "/" + includedFile;
-      File file = new File(filePath);
-      boolean isModified = file.lastModified() > latestBuildTime;
-      if(this.verbose==false) {
-        getLog().info(includedFile + " : " + (isModified ? "not up to date" : "up to date"));
-      }
-      domainSpecFileNames.add(filePath);
-      shouldCompile = shouldCompile || isModified;
+
+
+            String[] includedFiles = scanner.getIncludedFiles();
+            for (String includedFile : includedFiles) {
+                String filePath = this.srcDirectoryFile.getAbsolutePath() + "/" + includedFile;
+                File file = new File(filePath);
+                boolean isModified = file.lastModified() > latestBuildTime;
+                if (this.verbose == false) {
+                    getLog().info(includedFile + " : " + (isModified ? "not up to date" : "up to date"));
+                }
+                domainSpecFileNames.add(filePath);
+                shouldCompile = shouldCompile || isModified;
+            }
+
+        }
+        if(domainSpecFileNames.isEmpty()) {
+           getLog().info("No dml files found to generate domain");
+           return;
+        }
+        shouldCompile = true;
+        if (this.packageName == null) {
+            packageName = "";
+        }
+        if (shouldCompile) {
+            try {
+                destDirectoryBaseFile.setLastModified(System.currentTimeMillis());
+                if (this.verbose) {
+                    getLog().info("Using model: " + getDomainModelClass().getName());
+                    getLog().info("Using generator: " + getCodeGeneratorClass().getName());
+                }
+
+                compArgs = new CompilerArgs(this.destDirectoryFile, this.destDirectoryBaseFile, this.packageName, this.generateFinals, getCodeGeneratorClass(), getDomainModelClass(), domainSpecFileNames);
+
+                DomainModel model = DmlCompiler.getDomainModel(compArgs);
+
+                CodeGenerator generator = compArgs.getCodeGenerator().getConstructor(CompilerArgs.class, DomainModel.class).newInstance(compArgs, model);
+                generator.generateCode();
+                mavenProject.addCompileSourceRoot(destDirectoryBaseFile.getAbsolutePath());
+            } catch (Exception e) {
+                getLog().error(e);
+            }
+        } else {
+            if (this.verbose) {
+                getLog().info("All dml files are up to date. Skipping generation...");
+            }
+        }
     }
 
-    shouldCompile=true;
-    if(this.packageName==null) {
-      packageName="";
+    public Class<? extends CodeGenerator> getCodeGeneratorClass() throws ClassNotFoundException {
+        return (Class<? extends CodeGenerator>) Class.forName(this.codeGeneratorClassName);
     }
-    if (shouldCompile) {
-      try {
-           destDirectoryBaseFile.setLastModified(System.currentTimeMillis());
-           if(this.verbose) {
-             getLog().info("Using model: " + getDomainModelClass().getName());
-             getLog().info("Using generator: " + getCodeGeneratorClass().getName());
-           }
-           
-           compArgs = new CompilerArgs(this.destDirectoryFile, this.destDirectoryBaseFile, this.packageName, this.generateFinals, getCodeGeneratorClass(), getDomainModelClass(), domainSpecFileNames);
 
-           DomainModel model = DmlCompiler.getDomainModel(compArgs);
-
-           CodeGenerator generator = compArgs.getCodeGenerator().getConstructor(CompilerArgs.class, DomainModel.class).newInstance(compArgs, model);
-           generator.generateCode();
-		   mavenProject.addCompileSourceRoot(destDirectoryBaseFile.getAbsolutePath());
-         } catch (Exception e) {
-             getLog().error(e);
-         }
-       } else {
-         if(this.verbose) {
-           getLog().info("All dml files are up to date. Skipping generation...");
-         }
-       }
-     }
-
-  public Class<? extends CodeGenerator> getCodeGeneratorClass() throws ClassNotFoundException {
-    return (Class<? extends CodeGenerator>)Class.forName(this.codeGeneratorClassName);
-  }
-
-  public Class<? extends DomainModel> getDomainModelClass() throws ClassNotFoundException {
-    return (Class<? extends DomainModel>) Class.forName(this.domainModelClassName);
-  }
-
+    public Class<? extends DomainModel> getDomainModelClass() throws ClassNotFoundException {
+        return (Class<? extends DomainModel>) Class.forName(this.domainModelClassName);
+    }
 }
--- dml-maven-plugin/src/main/java/dml/maven/DmlMojoUtils.java
+++ dml-maven-plugin/src/main/java/dml/maven/DmlMojoUtils.java
@@ -1,30 +1,32 @@
 package dml.maven;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.project.MavenProject;
 
 public class DmlMojoUtils {
-    
+
     public static List<String> readDmlFilePathsFromArtifact(Log log, Set<Artifact> artifactSet) {
         List<String> dmlFilePaths = new ArrayList<String>();
-        for(Artifact artifact : artifactSet) {
-            if(artifact.getType().equals("pom") || (artifact.getGroupId().equals("pt.ist") && artifact.getArtifactId().equals("fenix-framework-core"))) {
+        for (Artifact artifact : artifactSet) {
+            if (artifact.getType().equals("pom") || (artifact.getGroupId().equals("pt.ist") && artifact.getArtifactId().equals("fenix-framework-core"))) {
                 continue; //ignoring pom projects and the fenix-framework-core project
             }
             String absolutePath = artifact.getFile().getAbsolutePath();
             try {
-               JarFile jarFile = new JarFile(absolutePath);
-               for(Enumeration<JarEntry> enumeration = jarFile.entries(); enumeration.hasMoreElements();) {
-                   JarEntry jarEntry = enumeration.nextElement();
-                   if(jarEntry.getName().endsWith(".dml")) {
-                       dmlFilePaths.add("jar:file:"+absolutePath+"!/"+jarEntry.getName());
-                   }
-               }
-            } catch(IOException e) {
+                JarFile jarFile = new JarFile(absolutePath);
+                for (Enumeration<JarEntry> enumeration = jarFile.entries(); enumeration.hasMoreElements();) {
+                    JarEntry jarEntry = enumeration.nextElement();
+                    if (jarEntry.getName().endsWith(".dml")) {
+                        dmlFilePaths.add("jar:file:" + absolutePath + "!/" + jarEntry.getName());
+                    }
+                }
+            } catch (IOException e) {
                 log.error(e);
             }
         }
