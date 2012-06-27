dcd9a3cd93035e4d2aedce8978541fed83860365
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2012-06-27 11:25:25 +0100

    * Changed external representation type of DomainObject from Object to   Serializable.

--- new-fenix-framework-core/src/main/java/pt/ist/fenixframework/DomainObject.java
+++ new-fenix-framework-core/src/main/java/pt/ist/fenixframework/DomainObject.java
@@ -1,6 +1,8 @@
 package pt.ist.fenixframework;
 
+import java.io.Serializable;
+
 public interface DomainObject {
-    public Object getExternalId();
-    public DomainObject fromExternalId(Object oid);
+    public Serializable getExternalId();
+    public DomainObject fromExternalId(Serializable oid);
 }
