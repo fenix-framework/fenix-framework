5643acaf11b447a492f10d4f14fee4f70c545da6
Pedro Santos <pedro.san7os@gmail.com>
2012-08-28 17:44:39 +0100

    Missing remove of service annotation

--- dml-maven-plugin/src/main/java/dml/maven/AbstractDmlPostProcessorMojo.java
+++ dml-maven-plugin/src/main/java/dml/maven/AbstractDmlPostProcessorMojo.java
@@ -7,25 +7,33 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+
 import jvstm.ProcessAtomicAnnotations;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+
 import pt.ist.fenixframework.artifact.FenixFrameworkArtifact;
 import pt.ist.fenixframework.project.DmlFile;
 
 public abstract class AbstractDmlPostProcessorMojo extends AbstractMojo {
 
     protected abstract MavenProject getMavenProject();
+
     protected abstract File getDmlSourceDirectory();
+
     protected abstract File getClassesDirectory();
+
     protected abstract String getCodeGeneratorClassName();
+
     protected abstract String getDomainModelClassName();
+
     protected abstract boolean verbose();
 
     @Override
     public void execute() throws MojoExecutionException {
-        if (getMavenProject().getArtifact().getType().equals("pom")) {
+	if (getMavenProject().getArtifact().getType().equals("pom")) {
 	    getLog().info("Cannot post process domain for pom projects");
 	    return;
 	}
@@ -58,10 +66,6 @@ public abstract class AbstractDmlPostProcessorMojo extends AbstractMojo {
 	    ProcessAtomicAnnotations atomicAnnotationsProcessor = new ProcessAtomicAnnotations(transactionClass,
 		    new String[] { "." });
 	    atomicAnnotationsProcessor.start();
-
-	    Class<?> serviceInjector = loader.loadClass("pt.ist.fenixframework.services.ServiceAnnotationInjector");
-	    Method injector = serviceInjector.getMethod("inject", File.class, ClassLoader.class);
-	    injector.invoke(null, this.getClassesDirectory(), loader);
 	} catch (Exception e) {
 	    getLog().error(e);
 	}
