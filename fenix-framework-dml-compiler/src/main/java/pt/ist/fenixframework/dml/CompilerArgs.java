e34ca5bc9ce137017185b5e3a71bf86f31cbf734
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2012-09-27 16:19:11 +0100

    Add support for project dependencies in DmlCompiler
      * Now, the DmlCompiler supports an additional parameter (-pn) to provide a
        project name instead of a list of local and external DML files. Using the
        fenix-framework-project module (and the appropriate project.properties
        files), the DmlCompiler identifies the local and external files
        automatically.  This greatly simplifies the invocation of the DmlCompiler
        from the command-line (e.g. in Ant build files).

--- fenix-framework-dml-compiler/src/main/java/pt/ist/fenixframework/dml/CompilerArgs.java
+++ fenix-framework-dml-compiler/src/main/java/pt/ist/fenixframework/dml/CompilerArgs.java
@@ -4,48 +4,69 @@ import java.io.*;
 import java.net.URL;
 import java.util.*;
 
+import pt.ist.fenixframework.core.DmlFile;
+import pt.ist.fenixframework.core.Project;
 import pt.ist.fenixframework.util.Converter;
 
 /**
  * Information about CompilerArgs:
  * 
  * <table border=1>
- * <tr><th>Type</th><th>Name</th><th>Description</th>
+ * <tr><th>Type</th><th>Name</th><th>Command-line flag</th><th>Description</th><th>Required</th>
  * <tr>
  *   <td><code>boolean</code></td> 
  *   <td><code>generateFinals</code></td>
+ *   <td><code>gf</code></td>
  *   <td> Whether to add the <code>final</code> keyword to the accessor methods.  Default: <code>false</code>.</td>
+ *   <td>No</td>
  * </tr>
  *
  * <tr>
  *   <td><code>File</code></td>
  *   <td><code>destDirectoryBase</code></td>
+ *   <td><code>db</code></td>
  *   <td>The top level directory where to generate the base classes.</td>
+ *   <td>No. Defaults to <code>destDirectory</code></td>
  * </tr>
  *
  * <tr><td><code>File</code></td>
  *   <td><code>destDirectory</code></td>
+ *   <td><code>d</code></td>
  *   <td> The top level directory where to generate the non-base classes.</td>
+ *   <td>Yes</td>
  * </tr>
  *
  * <tr><td><code>String</code></td>
  *   <td><code>packageName</code></td>
+ *   <td><code>p</code></td>
  *   <td>The default package name.  Default: <code>""</code>.</td>
+ *   <td>No</td>
  * </tr>
  *
  * <tr><td><code>List&lt;URL&gt;</code></td>
  *   <td><code>localDomainSpecs</code></td>
- *   <td>The list of project-local DML specifications.  The compiler will generate base classes (and non-base classes if they do not exist) for these DML specifications.  Default: empty list.</td>
+ *   <td><code>localDmlSpec</code></td>
+ *   <td>The list of project-local DML specifications.  For these DML specifications, the compiler will generate base classes (and non-base classes if they do not exist).  Default: empty list.</td>
+ *   <td rowspan="3">Must provide DML specifications either using <code>localDmlSpecs/externalDmlSpecs</code> or <code>projectName</code> </td>
  * </tr>
  *
  * <tr><td><code>List&lt;URL&gt;</code></td>
  *   <td><code>externalDomainSpecs</code></td>
- *   <td>The list of project-external DML specifications.  The compiler will only generate base classes for these DML specifications.  Default: empty list.</td>
+ *   <td><code>externalDmlSpec</code></td>
+ *   <td>The list of project-external DML specifications.  For these DML specifications, the compiler will generate base classes only.  Default: empty list.</td>
+ * </tr>
+ *
+ * <tr><td><code>String</code></td>
+ *   <td><code>projectName</code></td>
+ *   <td><code>pn</code></td>
+ *   <td>The name of the project.  The compiler will search for a &lt;projectName&gt;/project.properties file with the local DML files and other project dependencies to set the local/external specs.</td>
  * </tr>
  *
  * <tr><td><code>Class&lt;? extends CodeGenerator&gt;</code></td>
  *   <td><code>generatorClass</code></td>
+ *   <td><code>generator</code></td>
  *   <td> The code generator to use.  Default: <code>DefaultCodeGenerator.class</code>.</td>
+ *   <td>No</td>
  * </tr>
  *
  * </ul>
@@ -67,20 +88,49 @@ public class CompilerArgs {
      */
     private boolean addToLocalDomainSpecs = true; 
 
-    public CompilerArgs(File destDirectory, File destDirectoryBase, String packageName, Boolean generateFinals,
-	    Class<? extends CodeGenerator> generatorClass, List<URL> localDomainSpecs,
-                        List<URL> externalDomainSpecs) {
+    private CompilerArgs(File destDirectory, File destDirectoryBase, String packageName, Boolean generateFinals,
+	    Class<? extends CodeGenerator> generatorClass) {
 
 	this.destDirectory = destDirectory;
 	this.destDirectoryBase = destDirectoryBase;
 	this.packageName = packageName;
 	this.generateFinals = generateFinals;
 	this.generatorClass = generatorClass != null ? generatorClass : DefaultCodeGenerator.class;
+    }
+
+    /**
+     *  Create the CompilerArgs using a local and external DML files specification
+     */
+    public CompilerArgs(File destDirectory, File destDirectoryBase, String packageName,
+                        Boolean generateFinals, Class<? extends CodeGenerator> generatorClass,
+                        List<URL> localDomainSpecs, List<URL> externalDomainSpecs) {
+
+        this(destDirectory, destDirectoryBase, packageName, generateFinals, generatorClass);
 	if (localDomainSpecs != null) { this.localDomainSpecs.addAll(localDomainSpecs); }
         if (externalDomainSpecs != null) {this.externalDomainSpecs.addAll(externalDomainSpecs); }
 	checkArguments();
     }
 
+    /**
+     *  Create the CompilerArgs using the project name (requires a
+     *  <code>&lt;projName&gt;</code>/project.properties file in the classpath).
+     */
+    public CompilerArgs(File destDirectory, File destDirectoryBase, String packageName,
+                        Boolean generateFinals, Class<? extends CodeGenerator> generatorClass,
+                        String projectName) throws DmlCompilerException {
+
+        this(destDirectory, destDirectoryBase, packageName, generateFinals, generatorClass);
+        try {
+            setDmlSpecsFromProjectName(projectName);
+        } catch (Exception e) {
+            throw new DmlCompilerException(e);
+        }
+        checkArguments();
+    }
+
+    /**
+     *  Create the CompilerArgs using command-line args.
+     */
     public CompilerArgs(String[] args) throws DmlCompilerException {
         try {
             processCommandLineArgs(args);
@@ -98,7 +148,7 @@ public class CompilerArgs {
 	    error(destDirectory.toString() + " is not a readable directory");
 	}
 	if (localDomainSpecs.isEmpty() && externalDomainSpecs.isEmpty()) {
-	    error("no domainSpec files given (-localDmlSpec and/or -externalDmlSpec)");
+	    error("no domainSpec files given (-localDmlSpec and/or -externalDmlSpec) and no project name give (-pn).  Must provide one of the alternatives");
 	}
     }
 
@@ -141,6 +191,10 @@ public class CompilerArgs {
 
             addToDomainSpecs(getNextArgument(args, pos), addToLocalDomainSpecs);
             return pos + 2;
+	} else if (args[pos].equals("-pn")) {
+            String projectName = getNextArgument(args, pos);
+            setDmlSpecsFromProjectName(projectName);
+            return pos + 2;
         } else {
             // use last set default
             addToDomainSpecs(args[pos], addToLocalDomainSpecs);
@@ -171,6 +225,24 @@ public class CompilerArgs {
     void error(String msg) {
 	System.err.println("DmlCompiler: " + msg);
 	System.exit(1);
+    }
+    
+    private void setDmlSpecsFromProjectName(String projectName) throws Exception {
+        Project project = Project.fromName(projectName);
+        List<URL> allDmls = new ArrayList<URL>();
+        for (DmlFile dmlFile : project.getFullDmlSortedList()) {
+            allDmls.add(dmlFile.getUrl());
+        }
+        
+        List<URL> localDmls = new ArrayList<URL>();
+        for (DmlFile dmlFile : project.getDmls()) {
+            localDmls.add(dmlFile.getUrl());
+        }
+        List<URL> externalDmls = new ArrayList<URL>(allDmls);
+        externalDmls.removeAll(localDmls);
+
+        this.localDomainSpecs = localDmls;
+        this.externalDomainSpecs = externalDmls;
     }
 
     public Class<? extends CodeGenerator> getCodeGenerator() {
