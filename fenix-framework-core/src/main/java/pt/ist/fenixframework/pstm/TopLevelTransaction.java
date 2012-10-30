a192e24a8fea9fe8f1d9064f4abe57977c4dd527
Luis Cruz <luis.cruz@ist.utl.pt>
2012-10-30 15:08:11 +0000

    Allow VersionNotAvailableException to get passed a message that will be printed out with the stack trace.
    Identify object instance that causes the exception, and not only the
    class and attribute. This will allow easier "debugging" of corrupt data.
    
    Closes #31.

--- fenix-framework-core/src/main/java/pt/ist/fenixframework/pstm/TopLevelTransaction.java
+++ fenix-framework-core/src/main/java/pt/ist/fenixframework/pstm/TopLevelTransaction.java
@@ -300,8 +300,7 @@ public class TopLevelTransaction extends ConsistentTopLevelTransaction implement
 			// if not, then something gone wrong and its better to
 			// abort
 			if (body.value == VBox.NOT_LOADED_VALUE) {
-			    System.out.println("Couldn't load the attribute " + attr + " for class " + obj.getClass());
-			    throw new VersionNotAvailableException();
+			    throw new VersionNotAvailableException("Couldn't load the attribute " + attr + " for instance " + obj);
 			}
 		    }
 		}
