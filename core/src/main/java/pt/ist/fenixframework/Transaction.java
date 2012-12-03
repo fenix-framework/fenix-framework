2f7424cf1f5aeeb14141fedd986f5b3560f9af56
Ivo Anjo <ivo.anjo@ist.utl.pt>
2012-12-03 17:20:22 +0000

    Add mechanism for Transaction-local storage, and the TxIntrospector interface and implementation
      * Transaction-local variables are kept inside the Transaction.TxLocal class.
        This class also handles GC'ing old instances corresponding to finished
        transactions, via a worker thread that is automatically started.
      * Added the TxIntrospector interface to the
        pt.ist.fenixframework.txintrospector package. Compared to the old version
        from the JVSTM backend, it now returns values as Collections instead of
        Sets.
      * Finally, the TxStats class implements the new TxIntrospector interface,
        and serves also as a gathering point for the different information
        collected during a Transaction. An instance of TxStats is associated
        with a running transaction using the new Transaction-local mechanism.
    
    Work done in collaboration with Hugo Rito <hugo.rito@ist.utl.pt>

--- core/src/main/java/pt/ist/fenixframework/Transaction.java
+++ core/src/main/java/pt/ist/fenixframework/Transaction.java
@@ -1,5 +1,13 @@
 package pt.ist.fenixframework;
 
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.transaction.SystemException;
+
+import pt.ist.fenixframework.txintrospector.TxStats;
+
 public interface Transaction extends javax.transaction.Transaction {
     // public void commit();
     // // public boolean delistResource(XAResource xaRes, int flag);
@@ -8,4 +16,91 @@ public interface Transaction extends javax.transaction.Transaction {
     // // public void registerSynchronization(Synchronization sync);
     // public void rollback();
     // // public void setRollbackOnly();
+
+    /** TxLocal provides a generic storage place for Transaction-local objects **/
+    public static class TxLocal {
+
+        /**
+         * Key based on a WeakReference, to be used with the txToLocalMap.
+         * This design allows old TxLocal objects to be GC'd along with their corresponding Transaction
+         * objects.
+         */
+        private static final class MapKey extends WeakReference<javax.transaction.Transaction> {
+            private static final ReferenceQueue<javax.transaction.Transaction> REFERENCE_QUEUE =
+                    new ReferenceQueue<javax.transaction.Transaction>();
+
+            static {
+                // This thread monitors the reference queue and removes the TxLocal object corresponding
+                // to a GC'd transaction from the txToLocalMap
+                (new Thread(TxLocal.class.getName() + " GC Thread") {
+                    { setDaemon(true); }
+
+                    @Override
+                    public void run() {
+                        while (true) try {
+                            Object ref = REFERENCE_QUEUE.remove();
+                            while (TxLocal.txToLocalMap.remove(ref) != null);
+                        } catch (InterruptedException e) { throw new RuntimeException(e); }
+                    }
+                }).start();
+            }
+
+            private final int hashCode;
+
+            private MapKey(javax.transaction.Transaction transaction) {
+                super(transaction, REFERENCE_QUEUE);
+                hashCode = transaction.hashCode();
+            }
+
+            @Override
+            public int hashCode() {
+                return hashCode;
+            }
+
+            @Override
+            public boolean equals(Object other) {
+                if (other instanceof MapKey) {
+                    Object myObject = get();
+                    Object otherObject = ((MapKey) other).get();
+                    // NOTE: By design, two mapkeys originally pointing to different transactions are
+                    // considered equal once both are cleared (for GC purposes)
+                    return myObject == otherObject || (myObject != null && myObject.equals(otherObject));
+                }
+                return false;
+            }
+        }
+
+        private static final ConcurrentHashMap<MapKey, TxLocal> txToLocalMap =
+            new ConcurrentHashMap<MapKey, TxLocal>();
+
+        public static TxLocal getTxLocal() {
+            try {
+                return getTxLocal(FenixFramework.getTransactionManager().getTransaction());
+            } catch (SystemException se) {
+                throw new RuntimeException(se);
+            }
+        }
+
+        public static TxLocal getTxLocal(javax.transaction.Transaction transaction) {
+            MapKey mapKey = new MapKey(transaction);
+            TxLocal txLocal = txToLocalMap.get(mapKey);
+            if (txLocal == null) {
+                txLocal = new TxLocal();
+                TxLocal oldTxLocal = txToLocalMap.putIfAbsent(mapKey, txLocal);
+                if (oldTxLocal != null) {
+                    txLocal = oldTxLocal;
+                    mapKey.clear(); // not going to be needed anymore, help out GC
+                }
+            } else {
+                mapKey.clear(); // not going to be needed anymore, help out GC
+            }
+            return txLocal;
+        }
+
+        private TxLocal() { }
+
+        // Transaction-local fields
+        private final TxStats txStats = new TxStats();
+        public TxStats getTxStats() { return txStats; }
+    }
 }
---
+++ core/src/main/java/pt/ist/fenixframework/txintrospector/TxStats.java
@@ -0,0 +1,132 @@
+package pt.ist.fenixframework.txintrospector;
+
+import java.util.*;
+
+import pt.ist.fenixframework.DomainObject;
+import pt.ist.fenixframework.Transaction;
+import pt.ist.fenixframework.dml.runtime.Relation;
+import pt.ist.fenixframework.dml.runtime.RelationListener;
+
+public class TxStats implements TxIntrospector {
+
+    private static final boolean FILTER = Boolean.getBoolean("pt.ist.fenixframework.txintrospector.filter");
+
+    private final List<DomainObject> newObjects = new ArrayList<DomainObject>();
+    private final Set<DomainObject> modifiedObjects = new HashSet<DomainObject>();
+    private final Map<RelationChangelog, RelationChangelog> relationsChangelog =
+            new HashMap<RelationChangelog, RelationChangelog>();
+
+    public TxStats() { }
+
+    public void addNewObject(DomainObject object) {
+        if (filter(object)) return;
+
+        newObjects.add(object);
+    }
+
+    public void addModifiedObject(DomainObject object) {
+        if (filter(object)) return;
+
+        modifiedObjects.add(object);
+    }
+
+    public void addModifiedRelation(RelationChangelog relation) {
+        if (filter(relation.first)) return;
+
+        RelationChangelog prev = relationsChangelog.put(relation, relation);
+        if (prev != null && (prev.remove != relation.remove)) {
+            // Two changelog entries with opposite remove values cancel out
+            relationsChangelog.remove(relation);
+        }
+    }
+
+    // Filter out internal Fenix Framework structures
+    private boolean filter(DomainObject object) {
+        return FILTER && object.getClass().getPackage().getName().startsWith("pt.ist.fenixframework.core.adt.bplustree");
+    }
+
+    @Override
+    public String toString() {
+        return "TxStats\n\tnewObjects: " + getNewObjects()
+                + "\n\tdirectlyModifiedObjects: " + getDirectlyModifiedObjects()
+                + "\n\tmodifiedObjects: " + getModifiedObjects()
+                + "\n\trelationsChangelog: " + getRelationsChangelog();
+    }
+
+    // TxIntrospector implementation
+    @Override
+    public Collection<DomainObject> getNewObjects() {
+        return Collections.unmodifiableList(newObjects);
+    }
+
+    @Override
+    public Collection<DomainObject> getDirectlyModifiedObjects() {
+        Set<DomainObject> realModifiedObjects = new HashSet<DomainObject>(modifiedObjects);
+
+        // Any slot change triggers an object being added to modifiedObjects
+        // However, we want to separate new objects (even with modified slots) from existing objects
+        // that were modified, so we duplicate the modifiedObjects set, and remove from it the newObjects
+        realModifiedObjects.removeAll(newObjects);
+
+        return Collections.unmodifiableSet(realModifiedObjects);
+    }
+
+    @Override
+    public Collection<DomainObject> getModifiedObjects() {
+        Set<DomainObject> realModifiedObjects = new HashSet<DomainObject>(modifiedObjects);
+
+        // Add objects from relationship changes
+        for (RelationChangelog relationChange : relationsChangelog.values()) {
+            /*if (relationChange.first != null)*/ realModifiedObjects.add(relationChange.first);
+            /*if (relationChange.second != null)*/ realModifiedObjects.add(relationChange.second);
+        }
+
+        // Separate new objects from existing objects that were modified
+        realModifiedObjects.removeAll(newObjects);
+
+        return Collections.unmodifiableSet(realModifiedObjects);
+    }
+
+    @Override
+    public Collection<Entry> getReadSetLog() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Collection<Entry> getWriteSetLog() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Collection<RelationChangelog> getRelationsChangelog() {
+        return Collections.unmodifiableCollection(relationsChangelog.values());
+    }
+
+    // RelationListener used to track changes to relations
+    @SuppressWarnings("rawtypes")
+    public static final RelationListener STATS_LISTENER = new RelationListener<DomainObject,DomainObject>() {
+        @Override
+        public void beforeAdd(Relation<DomainObject,DomainObject> rel, DomainObject o1, DomainObject o2) {
+            if (o1 == null || o2 == null) return; // Ignore relations with null
+            Transaction.TxLocal.getTxLocal().getTxStats().addModifiedRelation(
+                    new RelationChangelog(rel.getName(), o1, o2, false));
+        }
+
+        @Override
+        public void afterAdd(Relation<DomainObject,DomainObject> rel, DomainObject o1, DomainObject o2) {
+            // intentionally empty
+        }
+
+        @Override
+        public void beforeRemove(Relation<DomainObject,DomainObject> rel, DomainObject o1, DomainObject o2) {
+            if (o1 == null || o2 == null) return; // Ignore relations with null
+            Transaction.TxLocal.getTxLocal().getTxStats().addModifiedRelation(
+                    new RelationChangelog(rel.getName(), o1, o2, true));
+        }
+
+        @Override
+        public void afterRemove(Relation<DomainObject,DomainObject> rel, DomainObject o1, DomainObject o2) {
+            // intentionally empty
+        }
+    };
+}
