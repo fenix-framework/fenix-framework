5c0f1638b3e5b22df5f7c2f714cb52bf4e3c1bbb
Ivo Anjo <ivo.anjo@ist.utl.pt>
2012-12-06 19:27:25 +0000

    Add hibernate-search module, that supports the indexing of domainobjects using hibernate-search
      * This module is responsible for using TxIntrospector to automatically index
        new and modified instances of classes marked with @Indexed during a
        transaction (and their relations).
      * User code can perform queries by using the SearchFactoryImplementor
        instance that can be obtained by calling
        HibernateSearchSupport.getSearchFactory().
      * The functionality from this module is only enabled at run-time if a
        configuration file for hibernate-search was provided.
    
    Work done in collaboration with Hugo Rito <hugo.rito@ist.utl.pt>

---
+++ hibernate-search/src/main/java/pt/ist/fenixframework/hibernatesearch/HibernateSearchConfig.java
@@ -0,0 +1,84 @@
+package pt.ist.fenixframework.hibernatesearch;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.TransactionListener;
+import pt.ist.fenixframework.dap.FFDAPConfig;
+
+public abstract class HibernateSearchConfig extends FFDAPConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(HibernateSearchConfig.class);
+
+    /**
+     * This <strong>optional</strong> parameter specifies the location of the properties file used
+     * to configure Hibernate Search.  This file should be available in the application's classpath.
+     */
+    protected String hibernateSearchConfigFile = null;
+
+    @Override
+    protected void init() {
+        super.init();
+
+        if (hibernateSearchConfigFile == null) {
+            logger.info("No property 'hibernateSearchConfigFile' given. Hibernate Search disabled");
+            return;
+        }
+        logger.info("Initializing Hibernate Search module");
+
+        URL hibernateSearchConfigURL = Thread.currentThread().getContextClassLoader().getResource(hibernateSearchConfigFile);
+        if (hibernateSearchConfigURL == null) {
+            logger.error("Cannot access resource '" + hibernateSearchConfigFile + "'. Hibernate Search disabled");
+            return;
+        }
+
+        logger.trace("Using Hibernate Search config file: " + hibernateSearchConfigURL);
+        Properties properties = new Properties();
+        try {
+            properties.load(hibernateSearchConfigURL.openStream());
+        } catch (IOException e) {
+            logger.error("Hibernate Search unable to create properties. Hibernate Search disabled", e);
+        }
+
+        HibernateSearchSupport.initializeSearchFactory(properties);
+
+        // Register our listener
+        FenixFramework.getTransactionManager().registerForBegin(new TransactionListener() {
+            @Override public void notifyBeforeBegin() { }
+
+            @Override public void notifyAfterBegin(Transaction tx) {
+                try {
+                    tx.registerSynchronization(new CommitIndexer());
+                } catch (IllegalStateException e) {
+                    logger.error("Exception caught in HibernateSearchConfig.init()", e);
+                } catch (RollbackException e) {
+                    logger.error("Exception caught in HibernateSearchConfig.init()", e);
+                } catch (SystemException e) {
+                    logger.error("Exception caught in HibernateSearchConfig.init()", e);
+                }
+            }
+        });
+    }
+
+    /**
+     * Subclasses of this class can overwrite this method, but they should specifically call
+     * <code>super.shutdown()</code> to orderly shutdown the framework and hibernate search.
+     */
+    @Override
+    public void shutdown() {
+        if (HibernateSearchSupport.getSearchFactory() != null) {
+            HibernateSearchSupport.getSearchFactory().close();
+        }
+
+        super.shutdown();
+    }
+}
