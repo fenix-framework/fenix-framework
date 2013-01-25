92fe25fc7bec6713ce447cdc0b4f2a61aa4ae44e
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-01-25 11:40:24 +0000

    Fixed Compilation Issues in the JVSTM-OJB Backend. Also add Eclipse Project Files

--- backend-jvstm-ojb/src/main/java/pt/ist/fenixframework/backend/jvstmojb/JvstmOJBTransactionManager.java
+++ backend-jvstm-ojb/src/main/java/pt/ist/fenixframework/backend/jvstmojb/JvstmOJBTransactionManager.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
+import javax.transaction.InvalidTransactionException;
 import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
@@ -11,20 +12,14 @@ import javax.transaction.Transaction;
 
 import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.CallableWithoutException;
-import pt.ist.fenixframework.TransactionManager;
+import pt.ist.fenixframework.core.AbstractTransactionManager;
 
-public class JvstmOJBTransactionManager implements TransactionManager {
+public class JvstmOJBTransactionManager extends AbstractTransactionManager {
 
     @Override
     public void backendCommit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SystemException {
 	// TODO Auto-generated method stub
 
-    }
-
-    @Override
-    public Transaction backendGetTransaction() throws SystemException {
-	// TODO Auto-generated method stub
-	return null;
     }
 
     @Override
@@ -34,25 +29,49 @@ public class JvstmOJBTransactionManager implements TransactionManager {
     }
 
     @Override
-    public <T> T backendWithTransaction(Callable<T> command) {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    @Override
-    public <T> T backendWithTransaction(Callable<T> command, Atomic atomic) {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    @Override
-    public void backendBegin(boolean readOnly) throws NotSupportedException, SystemException {
-	// TODO Auto-generated method stub
-
-    }
-
-    @Override
     public <T> T withTransaction(CallableWithoutException<T> command) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public pt.ist.fenixframework.Transaction getTransaction() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public <T> T withTransaction(Callable<T> command) throws Exception {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public <T> T withTransaction(Callable<T> command, Atomic atomic) throws Exception {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public void begin(boolean readOnly) throws NotSupportedException, SystemException {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume(Transaction tobj) throws InvalidTransactionException, IllegalStateException, SystemException {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setTransactionTimeout(int seconds) throws SystemException {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Transaction suspend() throws SystemException {
 	// TODO Auto-generated method stub
 	return null;
     }
