ff779f200c520fecfca2c1e3ded41f4b8bfbf4f5
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-02-04 16:30:26 +0000

    First Step towards backend-jvstm-ojb being compliant with the TransactionManager API.

---
+++ backend-jvstm-ojb/src/main/java/pt/ist/fenixframework/backend/jvstmojb/JvstmOJBTransaction.java
@@ -0,0 +1,64 @@
+package pt.ist.fenixframework.backend.jvstmojb;
+
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+
+import jvstm.CommitException;
+import pt.ist.fenixframework.core.AbstractTransaction;
+import pt.ist.fenixframework.core.CommitError;
+import pt.ist.fenixframework.pstm.Transaction;
+import pt.ist.fenixframework.txintrospector.TxIntrospector;
+
+public class JvstmOJBTransaction extends AbstractTransaction {
+
+	private final Transaction underlyingTransaction;
+
+	JvstmOJBTransaction(Transaction underlyingTransaction) {
+		super();
+		this.underlyingTransaction = underlyingTransaction;
+	}
+
+	Transaction getUnderlyingTransaction() {
+		return underlyingTransaction;
+	}
+
+	// AbstractTransaction implementations
+
+	@Override
+	protected void backendCommit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException,
+	SecurityException, IllegalStateException, SystemException {
+		if (Transaction.current() != underlyingTransaction) {
+			throw new IllegalStateException("JVSTM does not support committing transactions from other threads!");
+		}
+
+		try {
+			Transaction.commit();
+		} catch (CommitException e) {
+			throw new JvstmCommitError();
+		}
+	}
+
+	@Override
+	protected void backendRollback() throws IllegalStateException, SystemException {
+		if (Transaction.current() != underlyingTransaction) {
+			throw new IllegalStateException("JVSTM does not support committing transactions from other threads!");
+		}
+
+		Transaction.abort();
+	}
+
+	private static final class JvstmCommitError extends CommitError {
+		private static final long serialVersionUID = 2031232654005283536L;
+	}
+
+	// TxIntrospector
+
+	@Override
+	public TxIntrospector getTxIntrospector() {
+		// TODO Transaction is to implement TxIntrospector!
+		return null;
+	}
+
+}
--- backend-jvstm-ojb/src/main/java/pt/ist/fenixframework/backend/jvstmojb/JvstmOJBTransactionManager.java
+++ backend-jvstm-ojb/src/main/java/pt/ist/fenixframework/backend/jvstmojb/JvstmOJBTransactionManager.java
@@ -8,7 +8,11 @@ import javax.transaction.InvalidTransactionException;
 import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
-import javax.transaction.Transaction;
+
+import jvstm.Transaction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.CallableWithoutException;
@@ -16,68 +20,121 @@ import pt.ist.fenixframework.core.AbstractTransactionManager;
 
 public class JvstmOJBTransactionManager extends AbstractTransactionManager {
 
-    @Override
-    public void backendCommit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SystemException {
-	// TODO Auto-generated method stub
+	private static final Logger logger = LoggerFactory.getLogger(JvstmOJBTransactionManager.class);
 
-    }
+	/*
+	 * JvstmOJBTransactions are stored in a thread local, because JVSTM's are also.
+	 */
+	private final ThreadLocal<JvstmOJBTransaction> transactions = new ThreadLocal<JvstmOJBTransaction>();
 
-    @Override
-    public void backendRollback() throws SystemException {
-	// TODO Auto-generated method stub
+	@Override
+	public void begin(boolean readOnly) throws NotSupportedException {
+		if (transactions.get() != null) {
+			throw new NotSupportedException("Nesting is not yet supported in JVSTM based backends");
+		}
 
-    }
+		logger.trace("Begin Transaction. Read Only: {}", readOnly);
 
-    @Override
-    public <T> T withTransaction(CallableWithoutException<T> command) {
-	// TODO Auto-generated method stub
-	return null;
-    }
+		Transaction underlying = Transaction.begin(readOnly);
 
-    @Override
-    public pt.ist.fenixframework.Transaction getTransaction() {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    @Override
-    public <T> T withTransaction(Callable<T> command) throws Exception {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    @Override
-    public <T> T withTransaction(Callable<T> command, Atomic atomic) throws Exception {
-	pt.ist.fenixframework.pstm.Transaction.begin();
-	try {
-	    return command.call();
-	} finally {
-	    pt.ist.fenixframework.pstm.Transaction.commit();
+		transactions.set(new JvstmOJBTransaction((pt.ist.fenixframework.pstm.Transaction) underlying));
 	}
-    }
 
-    @Override
-    public void begin(boolean readOnly) throws NotSupportedException, SystemException {
-	// TODO Auto-generated method stub
+	@Override
+	public pt.ist.fenixframework.Transaction getTransaction() {
+		return transactions.get();
+	}
 
-    }
+	@Override
+	public void backendCommit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SystemException {
+		try {
+			logger.trace("Committing Transaction");
 
-    @Override
-    public void resume(Transaction tobj) throws InvalidTransactionException, IllegalStateException, SystemException {
-	// TODO Auto-generated method stub
+			// Note that no null check is needed, because a
+			// check has been made in the super-class.
+			transactions.get().commit();
+		} finally {
+			transactions.remove();
+		}
+	}
 
-    }
+	@Override
+	public void backendRollback() throws SystemException {
+		try {
+			logger.trace("Rolling Back Transaction");
 
-    @Override
-    public void setTransactionTimeout(int seconds) throws SystemException {
-	// TODO Auto-generated method stub
+			// Note that no null check is needed, because a
+			// check has been made in the super-class.
+			transactions.get().rollback();
+		} finally {
+			transactions.remove();
+		}
 
-    }
+	}
 
-    @Override
-    public Transaction suspend() throws SystemException {
-	// TODO Auto-generated method stub
-	return null;
-    }
+	@Override
+	public <T> T withTransaction(CallableWithoutException<T> command) {
+		try {
+			return withTransaction((Callable<T>) command);
+		} catch (Exception e) {
+			throw new RuntimeException("Exception ocurred while running transaction", e);
+		}
+	}
+
+	@Override
+	public <T> T withTransaction(Callable<T> command) throws Exception {
+		return withTransaction(command, null /* new AtomicInstance()*/);
+	}
+
+	@Override
+	public <T> T withTransaction(Callable<T> command, Atomic atomic) throws Exception {
+		if (pt.ist.fenixframework.pstm.Transaction.isInTransaction()) {
+			logger.trace("Flattening call to withTransaction - Already inside a transaction");
+			return command.call();
+		}
+
+		pt.ist.fenixframework.pstm.Transaction.begin();
+		try {
+			return command.call();
+		} finally {
+			pt.ist.fenixframework.pstm.Transaction.commit();
+		}
+	}
+
+	@Override
+	public void resume(javax.transaction.Transaction tobj) throws InvalidTransactionException, IllegalStateException,
+	SystemException {
+		if (!(tobj instanceof JvstmOJBTransaction)) {
+			throw new InvalidTransactionException("Expected JvstmOJBTransaction, got " + tobj);
+		}
+
+		if (transactions.get() != null) {
+			throw new IllegalStateException("Already associated with a transaction!");
+		}
+
+		JvstmOJBTransaction tx = (JvstmOJBTransaction) tobj;
+
+		Transaction.resume(tx.getUnderlyingTransaction());
+		transactions.set(tx);
+	}
+
+	@Override
+	public JvstmOJBTransaction suspend() {
+		JvstmOJBTransaction current = transactions.get();
+
+		if (current == null) {
+			return current;
+		}
+
+		Transaction.suspend();
+		transactions.remove();
+
+		return current;
+	}
+
+	@Override
+	public void setTransactionTimeout(int seconds) throws SystemException {
+		throw new UnsupportedOperationException("Transaction timeouts are not supported in JVSTM");
+	}
 
 }
