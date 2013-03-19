e064c25f7915958b33a1f4198f7f4c2a063f3d3a
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-03-19 17:14:29 +0000

    Added Service Loader infrastructure to support Consistency Predicates   * Added Service declaration   * Added implementation of the service to the JVSTM/OJB Backend

---
+++ backend-jvstm-ojb/src/main/java/pt/ist/fenixframework/backend/jvstmojb/JvstmOJBConsistencyPredicateSupport.java
@@ -0,0 +1,7 @@
+package pt.ist.fenixframework.backend.jvstmojb;
+
+import pt.ist.fenixframework.consistencyPredicates.ConsistencyPredicateSupport;
+
+public class JvstmOJBConsistencyPredicateSupport extends ConsistencyPredicateSupport {
+
+}
---
+++ consistency-predicates/src/main/java/pt/ist/fenixframework/consistencyPredicates/ConsistencyPredicateSupport.java
@@ -0,0 +1,38 @@
+package pt.ist.fenixframework.consistencyPredicates;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class ConsistencyPredicateSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsistencyPredicateSupport.class);
+
+    private static final class Holder {
+
+        private static final ConsistencyPredicateSupport instance = resolveInstance();
+
+        private static ConsistencyPredicateSupport resolveInstance() {
+            Iterator<ConsistencyPredicateSupport> iterator = ServiceLoader.load(ConsistencyPredicateSupport.class).iterator();
+            if (!iterator.hasNext()) {
+                throw new Error("No implementations of " + ConsistencyPredicateSupport.class.getName()
+                        + " were found in the classpath");
+            }
+            ConsistencyPredicateSupport support = iterator.next();
+            if (iterator.hasNext()) {
+                logger.error("More than one instance of {} was found in the classpath\nUsing {}",
+                        ConsistencyPredicateSupport.class, support);
+            }
+            logger.debug("ConsistencyPredicateSupport is provided by {}", support);
+            return support;
+        }
+
+    }
+
+    static ConsistencyPredicateSupport getInstance() {
+        return Holder.instance;
+    }
+
+}
