74306eaec198e97c540f944c4a215d12cd55af41
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2013-06-13 10:38:48 +0100

    [jvstm-common] Implement StandaloneVBox.reload() and makeNew()

--- backend/jvstm-common/runtime/src/main/java/pt/ist/fenixframework/backend/jvstm/pstm/StandaloneVBox.java
+++ backend/jvstm-common/runtime/src/main/java/pt/ist/fenixframework/backend/jvstm/pstm/StandaloneVBox.java
@@ -1,9 +1,13 @@
 package pt.ist.fenixframework.backend.jvstm.pstm;
 
+import jvstm.Transaction;
 import jvstm.VBoxBody;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import pt.ist.fenixframework.backend.jvstm.JVSTMBackEnd;
+import pt.ist.fenixframework.backend.jvstm.repository.PersistenceException;
 
 public class StandaloneVBox<E> extends VBox<E> {
 
@@ -33,12 +37,39 @@ public class StandaloneVBox<E> extends VBox<E> {
 
     @Override
     protected void doReload() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("not yet implemented");
+        logger.debug("Reload StandaloneVBox: {}", this.getId());
+
+        try {
+            JVSTMBackEnd.getInstance().getRepository().reloadAttribute(this);
+        } catch (PersistenceException e) {
+            VBoxBody<E> body = getBody(Transaction.current().getNumber());
+            if (body != null && body.version == 0) {
+                logger.debug("VBox is new. Setting null in body with version 0.");
+                body.value = null;
+            } else {
+                String message = "BUG: After reload body is " + (body == null ? "null" : "not null");
+                if (body != null) {
+                    message += ". version=" + body.version;
+                }
+                logger.error(message);
+                throw e;
+            }
+        }
     }
 
     public static StandaloneVBox lookupCachedVBox(String vboxId) {
         return (StandaloneVBox) VBoxCache.getCache().lookup(vboxId);
+    }
+
+    public static <T> StandaloneVBox<T> makeNew(String vboxId, boolean allocateOnly) {
+        if (allocateOnly) {
+            // when a box is allocated, it is safe to say that the version number is 0
+            return new StandaloneVBox<T>(vboxId, makeNewBody((T) NOT_LOADED_VALUE, 0, null));
+        } else {
+            StandaloneVBox<T> vbox = new StandaloneVBox<T>(vboxId);
+            vbox.put(null); // this is required to add the vbox to the writeset
+            return vbox;
+        }
     }
 
 }
