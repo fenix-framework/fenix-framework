9532c83ac415ba618d2d0e2c40ce270a2d3b5edb
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2013-06-27 12:05:47 +0100

    Support setting B+Tree LOWER_BOUND via configuration
      Use config property 'btreeLowerBound'.  This affects both BPlusTree and
      BPlusTreeArray implementations.

--- core/src/main/java/pt/ist/fenixframework/Config.java
+++ core/src/main/java/pt/ist/fenixframework/Config.java
@@ -1,14 +1,11 @@
 package pt.ist.fenixframework;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,56 +17,56 @@ import pt.ist.fenixframework.core.exception.ProjectException;
 import pt.ist.fenixframework.util.Converter;
 
 /**
- * <p> An instance of the <code>Config</code> class bundles together the initialization parameters
- * used by the Fenix Framework.
- *
- * Therefore, before initializing the framework (via the call to the
- * {@link FenixFramework#initialize(Config)} method), the programmer should create an instance of
- * <code>Config</code> with the correct values for each of the parameters.</p>
+ * <p>
+ * An instance of the <code>Config</code> class bundles together the initialization parameters used by the Fenix Framework.
  * 
- * <p> No constructor is provided for this class (other than the default constructor), because the
- * <code>Config</code> class has several parameters, some of which are optional. But, whereas
- * optional parameters do not need to be specified, the parameters that are required must be
- * specified by the programmer before calling the {@link FenixFramework#initialize(Config)} method.
- *
- * <p> Additional configuration parameters may be added by subclassing this class.  Subclasses of
- * config can override the {@link #init()} method.  Typically, their own {@link #init()} should also
- * call {@link super#init()} if an hierarchy of configs is used.
+ * Therefore, before initializing the framework (via the call to the {@link FenixFramework#initialize(Config)} method), the
+ * programmer should create an instance of <code>Config</code> with the correct values for each of the parameters.
+ * </p>
  * 
- * <p> To create an instance of this class with the proper values for its parameters, programmers
- * should generally use code like this (assuming one specific backend as an example):
+ * <p>
+ * No constructor is provided for this class (other than the default constructor), because the <code>Config</code> class has
+ * several parameters, some of which are optional. But, whereas optional parameters do not need to be specified, the parameters
+ * that are required must be specified by the programmer before calling the {@link FenixFramework#initialize(Config)} method.
+ * 
+ * <p>
+ * Additional configuration parameters may be added by subclassing this class. Subclasses of config can override the
+ * {@link #init()} method. Typically, their own {@link #init()} should also call {@link super#init()} if an hierarchy of configs
+ * is used.
+ * 
+ * <p>
+ * To create an instance of this class with the proper values for its parameters, programmers should generally use code like this
+ * (assuming one specific backend as an example):
  * 
  * <blockquote>
  * 
  * <pre>
  * 
  * Config config = new MemConfig() { // any subclass of Config should be ok
- *     {
- * 	this.domainModelURLs = resourceToURLArray(&quot;path/to/domain.dml&quot;);
- * 	this.appName = &quot;MyAppName&quot;;
- *     }
- * };
+ *             {
+ *                 this.domainModelURLs = resourceToURLArray(&quot;path/to/domain.dml&quot;);
+ *                 this.appName = &quot;MyAppName&quot;;
+ *             }
+ *         };
  * 
  * </pre>
- *
+ * 
  * </blockquote>
  * 
- * Note that the <code>Config</code> takes an array of URLs for the <code>domainModelURLs</code>.
- * Utility methods are provided in this class to convert from other typical representations to their
- * corresponding URL.  All of the utility method return the required <code>URL[]</code>.  Here are
- * some guidelines:
- *
+ * Note that the <code>Config</code> takes an array of URLs for the <code>domainModelURLs</code>. Utility methods are provided in
+ * this class to convert from other typical representations to their corresponding URL. All of the utility method return the
+ * required <code>URL[]</code>. Here are some guidelines:
+ * 
  * <ul>
- *
+ * 
  * <li>{@link #resourceToURLArray(String)}: looks up the given resource using
  * <code>Thread.currentThread().getContextClassLoader().getResource(String)</code>;</li>
- *
- * <li>{@link #filenameToURLArray(String)}: looks up the given file on the local filesystem;
- * </li>
- *
- * <li>Methods using the plural form (e.g. {@link #resourcesToURLArray(String [])}) are equivalent,
- * except that they take multiple DML file locations.</li>
- *
+ * 
+ * <li>{@link #filenameToURLArray(String)}: looks up the given file on the local filesystem;</li>
+ * 
+ * <li>Methods using the plural form (e.g. {@link #resourcesToURLArray(String [])}) are equivalent, except that they take multiple
+ * DML file locations.</li>
+ * 
  * </ul>
  * 
  * Note the use of the double
@@ -78,13 +75,10 @@ import pt.ist.fenixframework.util.Converter;
  * { {} }
  * </pre>
  * 
- * to delimit an instance initializer block for the anonymous inner class being
- * created.
+ * to delimit an instance initializer block for the anonymous inner class being created.
  * 
- * Each of the parameters of the <code>Config</code> class is represented as a
- * protected class field. Look at the documentation of each field to see what is
- * it for, whether it is optional or required, and in the former case, what is
- * its default value.
+ * Each of the parameters of the <code>Config</code> class is represented as a protected class field. Look at the documentation of
+ * each field to see what is it for, whether it is optional or required, and in the former case, what is its default value.
  * 
  * @see pt.ist.fenixframework.FenixFramework
  * @see pt.ist.fenixframework.backend.mem.MemConfig
@@ -93,7 +87,8 @@ import pt.ist.fenixframework.util.Converter;
 public abstract class Config {
     private static final Logger logger = LoggerFactory.getLogger(Config.class);
 
-    protected static final String NO_DOMAIN_MODEL = "Fenix Framework cannot proceed without a domain model!.  Either provide \"appName\" (for which there is a project.properties file) or explicitly set \"domainModelURLs\".";
+    protected static final String NO_DOMAIN_MODEL =
+            "Fenix Framework cannot proceed without a domain model!.  Either provide \"appName\" (for which there is a project.properties file) or explicitly set \"domainModelURLs\".";
 
     protected static final String PROPERTY_CONFIG_CLASS = "config.class";
     // the suffix of the method that sets a property from a String property
@@ -107,9 +102,9 @@ public abstract class Config {
     protected URL[] domainModelURLs = null;
 
     /**
-     * This <strong>optional</strong> parameter specifies a name for the application.  When using
-     * configuration by convention, this name (if set) is used to lookup the
-     * <code>&lt;appName&gt;/project.properties</code> file.  Additionally, this name will be used
+     * This <strong>optional</strong> parameter specifies a name for the application. When using
+     * configuration by convention, this name (if set) is used to lookup the <code>&lt;appName&gt;/project.properties</code> file.
+     * Additionally, this name will be used
      * by the framework in the statistical logs performed during the application execution. The
      * default value for this parameter is <code>null</code>.
      */
@@ -117,48 +112,54 @@ public abstract class Config {
 
     /**
      * This <strong>optional</strong> parameter specifies the number of nodes that are expected to
-     * be deployed.  This can be used by the backends to perform some setup, e.g. to wait for some
-     * number of nodes to become live in order to complete the initialization process.  The default
+     * be deployed. This can be used by the backends to perform some setup, e.g. to wait for some
+     * number of nodes to become live in order to complete the initialization process. The default
      * value for this parameter is <code>1</code>.
      */
     protected int expectedInitialNodes = 1;
 
     /**
-     * This <strong>optional</strong> parameter specifies the JGroups configuration file.  This
-     * configuration will used to create channels between Fenix Framework nodes.  The default value
+     * This <strong>optional</strong> parameter specifies the JGroups configuration file. This
+     * configuration will used to create channels between Fenix Framework nodes. The default value
      * for this parameter is <code>fenix-framework-udp-jgroups.xml</code>, which is the default
      * configuration file that ships with the framework.
      */
     protected String jGroupsConfigFile = "fenix-framework-udp-jgroups.xml";
 
+    /**
+     * This <strong>optional</strong> parameter specifies the LOWER_BOUND constant used by B+Trees. The default value is 100. The
+     * minimum value is 2.
+     */
+    protected int btreeLowerBound = 100;
+
     protected void checkRequired(Object obj, String fieldName) {
-	if (obj == null) {
-	    missingRequired(fieldName);
-	}
+        if (obj == null) {
+            missingRequired(fieldName);
+        }
     }
 
     /**
      * Check if the value of <code>domainModelURLs</code> is already set.
      */
     protected void checkForDomainModelURLs() {
-	if ((domainModelURLs == null) || (domainModelURLs.length == 0)) {
+        if ((domainModelURLs == null) || (domainModelURLs.length == 0)) {
             logger.error(NO_DOMAIN_MODEL);
-	    missingRequired("domainModelURLs");
-	}
+            missingRequired("domainModelURLs");
+        }
     }
 
     /**
-     * Subclasses of this class can overwrite this method, but they should specifically call
-     * <code>super.checkConfig()</code> to check the superclass's configuration.
+     * Subclasses of this class can overwrite this method, but they should specifically call <code>super.checkConfig()</code> to
+     * check the superclass's configuration.
      */
     protected void checkConfig() {
         checkForDomainModelURLs();
     }
-    
+
     protected static void missingRequired(String fieldName) {
-	throw new ConfigError(ConfigError.MISSING_REQUIRED_FIELD, "'" + fieldName + "'");
+        throw new ConfigError(ConfigError.MISSING_REQUIRED_FIELD, "'" + fieldName + "'");
     }
-    
+
     /**
      * This method is invoked by the {@link FenixFramework#initialize(Config)}.
      */
@@ -171,7 +172,9 @@ public abstract class Config {
     // define which config instance to create
     protected final void populate(Properties props) {
         for (String propName : props.stringPropertyNames()) {
-            if (PROPERTY_CONFIG_CLASS.equals(propName)) { continue; }
+            if (PROPERTY_CONFIG_CLASS.equals(propName)) {
+                continue;
+            }
             String value = props.getProperty(propName);
             setProperty(propName, value);
         }
@@ -190,11 +193,11 @@ public abstract class Config {
         }
 
         // note that lazy evaluation is used on purpose!
-        boolean success = attemptSetPropertyUsingMethod(getSetterFor(this.getClass(), propName +
-                                                                     SETTER_FROM_STRING), value) ||
-            attemptSetPropertyUsingField(field, value);
+        boolean success =
+                attemptSetPropertyUsingMethod(getSetterFor(this.getClass(), propName + SETTER_FROM_STRING), value)
+                        || attemptSetPropertyUsingField(field, value);
 
-        if (! success) {
+        if (!success) {
             throw new ConfigError(ConfigError.COULD_NOT_SET_PROPERTY, propName);
         }
     }
@@ -206,7 +209,7 @@ public abstract class Config {
             // climb the hierarchy, but only up to Config class
             Class<?> superclass = clazz.getSuperclass();
             if (Config.class.isAssignableFrom(superclass)) {
-                return getField((Class<? extends Config>)superclass, fieldName);
+                return getField((Class<? extends Config>) superclass, fieldName);
             } else {
                 throw new ConfigError(ConfigError.UNKNOWN_PROPERTY + fieldName, e);
             }
@@ -214,8 +217,10 @@ public abstract class Config {
     }
 
     private boolean attemptSetPropertyUsingMethod(Method setter, String value) {
-        if (setter == null) return false;
-        
+        if (setter == null) {
+            return false;
+        }
+
         setter.setAccessible(true);
         try {
             setter.invoke(this, value);
@@ -237,12 +242,12 @@ public abstract class Config {
 
     private Method getSetterFor(Class<? extends Config> clazz, String setterName) {
         try {
-            return clazz.getDeclaredMethod(setterName, new Class[]{String.class});
+            return clazz.getDeclaredMethod(setterName, new Class[] { String.class });
         } catch (NoSuchMethodException e) {
             // climb the hierarchy, but only up to Config class
             Class<?> superclass = clazz.getSuperclass();
             if (Config.class.isAssignableFrom(superclass)) {
-                return getSetterFor((Class<? extends Config>)superclass, setterName);
+                return getSetterFor((Class<? extends Config>) superclass, setterName);
             } else {
                 return null;
             }
@@ -255,7 +260,6 @@ public abstract class Config {
             throw new ConfigError(ConfigError.DUPLICATE_DEFINITION_OF_DOMAIN_MODEL_URLS);
         }
     }
-
 
     /**
      * Note: Either appNameFromString or domainModelURLsFromString should be used, but not both!
@@ -294,11 +298,15 @@ public abstract class Config {
         for (int i = 0; i < tokens.length; i++) {
             // try URL from resource
             urls[i] = Converter.resourceToURL(tokens[i]);
-            if (urls[i] != null) { continue; }
+            if (urls[i] != null) {
+                continue;
+            }
 
             // try URL from filename
             urls[i] = Converter.filenameToURL(tokens[i]);
-            if (urls[i] != null) { continue; }
+            if (urls[i] != null) {
+                continue;
+            }
 
             // failed to get the URL
             throw new Error("FenixFramework config error: cannot find DML '" + tokens[i] + "'");
@@ -309,6 +317,14 @@ public abstract class Config {
     protected void expectedInitialNodesFromString(String value) {
         try {
             expectedInitialNodes = Integer.parseInt(value.trim());
+        } catch (NumberFormatException e) {
+            throw new ConfigError(e);
+        }
+    }
+
+    public void btreeLowerBoundFromString(String value) {
+        try {
+            btreeLowerBound = Integer.parseInt(value.trim());
         } catch (NumberFormatException e) {
             throw new ConfigError(e);
         }
@@ -334,7 +350,7 @@ public abstract class Config {
     public abstract BackEnd getBackEnd();
 
     public URL[] getDomainModelURLs() {
-	return domainModelURLs;
+        return domainModelURLs;
     }
 
     public int getExpectedInitialNodes() {
@@ -346,12 +362,16 @@ public abstract class Config {
     }
 
     public String getAppName() {
-	return appName;
+        return appName;
+    }
+
+    public int getBtreeLowerBound() {
+        return btreeLowerBound;
     }
 
     /**
-     * Subclasses of this class can overwrite this method, but they should specifically call
-     * <code>super.shutdown()</code> to orderly shutdown the framework.
+     * Subclasses of this class can overwrite this method, but they should specifically call <code>super.shutdown()</code> to
+     * orderly shutdown the framework.
      */
     protected void shutdown() {
         getBackEnd().shutdown();
@@ -366,7 +386,7 @@ public abstract class Config {
         return Converter.resourceToURLArray(resource);
     }
 
-    public static URL[] resourcesToURLArray(String ... resources) {
+    public static URL[] resourcesToURLArray(String... resources) {
         return Converter.resourcesToURLArray(resources);
     }
 
@@ -376,7 +396,7 @@ public abstract class Config {
         return Converter.filenameToURLArray(filename);
     }
 
-    public static URL[] filenamesToURLArray(String ... filenames) {
+    public static URL[] filenamesToURLArray(String... filenames) {
         return Converter.filenamesToURLArray(filenames);
     }
 
