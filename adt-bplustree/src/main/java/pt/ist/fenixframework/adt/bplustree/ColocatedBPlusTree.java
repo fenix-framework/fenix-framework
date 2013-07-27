730df1667a59df07fb3589cd0d06b572d269c118
nmldiegues <nmld@ist.utl.pt>
2013-07-27 12:16:59 +0100

    Changed new collections to inherit from _Base classes

--- adt-bplustree/src/main/java/pt/ist/fenixframework/adt/bplustree/ColocatedBPlusTree.java
+++ adt-bplustree/src/main/java/pt/ist/fenixframework/adt/bplustree/ColocatedBPlusTree.java
@@ -8,8 +8,12 @@ import pt.ist.fenixframework.dml.runtime.DomainBasedMap;
 import eu.cloudtm.Constants;
 import eu.cloudtm.LocalityHints;
 
-public class ColocatedBPlusTree<T extends Serializable>  extends BPlusTree<T> {
+public class ColocatedBPlusTree<T extends Serializable> extends ColocatedBPlusTree_Base {
 
+    public ColocatedBPlusTree() {
+	// this is not used by the code generator
+    }
+    
     public ColocatedBPlusTree(String relationName) {
 	super(new LocalityHints(new String[]{Constants.GROUP_ID, UUID.randomUUID().toString() + DomainBasedMap.RELATION_NAME_SEPARATOR + relationName}));
     }
--- adt-bplustree/src/main/java/pt/ist/fenixframework/adt/bplustree/ColocatedBPlusTreeGhost.java
+++ adt-bplustree/src/main/java/pt/ist/fenixframework/adt/bplustree/ColocatedBPlusTreeGhost.java
@@ -8,8 +8,12 @@ import pt.ist.fenixframework.dml.runtime.DomainBasedMap;
 import eu.cloudtm.Constants;
 import eu.cloudtm.LocalityHints;
 
-public class ColocatedBPlusTreeGhost<T extends Serializable>  extends BPlusTreeGhost<T> {
+public class ColocatedBPlusTreeGhost<T extends Serializable> extends ColocatedBPlusTreeGhost_Base {
 
+    public ColocatedBPlusTreeGhost() {
+	// this is not used by the code generator
+    }
+    
     public ColocatedBPlusTreeGhost(String relationName) {
 	super(new LocalityHints(new String[]{Constants.GROUP_ID, UUID.randomUUID().toString() + DomainBasedMap.RELATION_NAME_SEPARATOR + relationName}));
     }
