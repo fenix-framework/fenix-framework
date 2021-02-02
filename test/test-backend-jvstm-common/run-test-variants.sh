b3e792c2e18ccac776b37249b5a21924903e2578
Marco Lopes Cerdeira <marco.cerdeira@tecnico.ulisboa.pt>
2021-02-02 22:30:31 +0000

    Resolve "Remove modules not used in dsi"

--- test/test-backend-jvstm-common/run-test-variants.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-\mvn clean test -DforkCount=1.5C -Dcode.generator.class=pt.ist.fenixframework.backend.jvstm.JVSTMCodeGenerator && \
-    \mvn clean test -DforkCount=1 -Dcode.generator.class=pt.ist.fenixframework.backend.jvstm.infinispan.JvstmIspnCodeGenerator -Dtest=test.backend.jvstm.ConcurrentUpdatesTest,test.backend.jvstm.SequentialUpdatesTest,test.backend.jvstm.SimpleTest
--- test/test-backend-jvstm-common/src/test/java/test/backend/jvstm/domain/ValueTypes.java
+++
@@ -1,9 +0,0 @@
-package test.backend.jvstm.domain;
-
-public class ValueTypes extends ValueTypes_Base {
-
-    public ValueTypes() {
-        super();
-    }
-
-}
